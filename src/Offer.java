8273e4ac2213cdb929c37c8b3377594836aa3ead
fedlxs <88156769+fedlxs@users.noreply.github.com>
2022-12-03 07:38:30 +0100

    Initial classes

---
+++ src/Offer.java
@@ -0,0 +1,21 @@
+public class Offer {
+    public Offer (String name, String location)
+    {
+      this.name = name;
+      this.location = location;
+
+    }
+
+//    private String
+    private String name;
+    private String location;
+
+    public String getName ()
+    {
+        return name;
+    }
+    public String getLocation ()
+    {
+        return location;
+    }
+}
